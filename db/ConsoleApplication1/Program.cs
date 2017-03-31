bcbd78ef73701eb0f712a51bd25394f84eeb7da2
user <user@ACER>
2017-03-31 20:27:03 +0800

    db_write

--- db/ConsoleApplication1/Program.cs
+++ db/ConsoleApplication1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using db.model;
+using db.service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +13,36 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             var importservice = new db.service.import();
-           // var station = importservice.FindStations(@"F:\water_info.xml");
+            var db = new db.Repository.stationrep();
 
+            var stations = import.FindStations(@"F:\water_info.xml");
 
+            stations
+              .ToList().ForEach(station =>
+            {
+             db.Create(stations);
+            });
+
+            // var station = importservice.FindStations(@"F:\water_info.xml");
+            // var stations = db.FindAllStations();
+
+            //  ShowStation(stations);
+
+            // Console.ReadKey(); //暫停
         }
+
+        /*  public static void ShowStation(List<Station> stations)
+          {
+
+              Console.WriteLine(string.Format("共收到{0}筆監測站的資料", stations.Count));
+              stations.ForEach(x =>
+              {
+                  Console.WriteLine(string.Format("站點名稱：{0},地址:{1}", x.ObservatoryName, x.LocationAddress));
+
+
+              });
+
+
+          }*/
     }
 }
---
+++ db/db/Repository/stationrep.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db.Repository
+{
+    public class stationrep
+    {
+        private const string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\Desktop\homework-master\db\ConsoleApplication1\db_data\water_data.mdf;Integrated Security=True";
+
+
+        public void Create(List<model.Station> stations)
+        {
+            var connection = new System.Data.SqlClient.SqlConnection();
+            connection.ConnectionString = _connectionString;
+            connection.Open();
+
+            foreach (var station in stations)
+            {
+                var command = new System.Data.SqlClient.SqlCommand("", connection);
+                command.CommandText = string.Format(@"
+INSERT        INTO    water(LocationAddress, ObservatoryName, LocationByTWD67, CreateTime)
+VALUES          (N'{0}',N'{1}',N'{2}',N'{3}')
+", station.LocationAddress, station.ObservatoryName, station.LocationByTWD67, station.CreateTime.ToString("yyyy/MM/dd"));
+
+                command.ExecuteNonQuery();
+            }
+            connection.Close();
+        }
+    }
+}
